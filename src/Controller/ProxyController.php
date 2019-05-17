7f76661d1c1eef672a4849ea98054221cf73e859
Peter Ton <mail@peterton.nl>
2019-05-17 11:05:00 +0200

    Implement test redirect for post request

--- src/Controller/ProxyController.php
+++ src/Controller/ProxyController.php
@@ -2,12 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\RedirectController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class ProxyController
+class ProxyController extends RedirectController
 {
-    public function index()
+    public function index(Request $request): Response
     {
-        return new JsonResponse('hello world');
+        return $this->urlRedirectAction(
+            $request,
+            'http://127.0.0.1:8000/test/-/npm/v1/security/audits',
+            false,
+            null,
+            null,
+            null,
+            true
+        );
+    }
+    public function test(): Response
+    {
+        return new JsonResponse('hello test');
     }
 }
