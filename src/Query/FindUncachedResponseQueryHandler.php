6d0f8d4d65b633c2554707f2bd869a92a6ae3af3
Peter Ton <mail@peterton.nl>
2019-05-24 17:12:34 +0200

    Add logging in case a non expected response from registry is returned

--- src/Query/FindUncachedResponseQueryHandler.php
+++ src/Query/FindUncachedResponseQueryHandler.php
@@ -7,6 +7,7 @@ use App\Factory\UncachedResponseFactory;
 use App\Mapper\RequestMapper;
 use GuzzleHttp\Client as GuzzleClient;
 use GuzzleHttp\Exception\GuzzleException;
+use Psr\Log\LoggerInterface;
 
 class FindUncachedResponseQueryHandler
 {
@@ -26,14 +27,21 @@ class FindUncachedResponseQueryHandler
      */
     private $uncachedResponseFactory;
 
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
     public function __construct(
         RequestMapper $requestMapper,
         GuzzleClient $client,
-        UncachedResponseFactory $uncachedResponseFactory
+        UncachedResponseFactory $uncachedResponseFactory,
+        LoggerInterface $logger
     ) {
         $this->requestMapper = $requestMapper;
         $this->client = $client;
         $this->uncachedResponseFactory = $uncachedResponseFactory;
+        $this->logger = $logger;
     }
 
     public function handle(FindUncachedResponseQuery $query): ?UncachedResponse
@@ -43,6 +51,12 @@ class FindUncachedResponseQueryHandler
             $response = $this->client->send($guzzleRequest, ['timeout' => 10]);
             return $this->uncachedResponseFactory->createFromResponse($response);
         } catch (GuzzleException $e) {
+            if ($e->getCode() !== 503) {
+                $this->logger->warning('Error retrieving uncached response: "{error}"', [
+                    'error' => $e->getMessage(),
+                    'trace' => $e->getTrace()
+                ]);
+            }
             return null;
         }
     }
